b3e449199bbe6b9b624e0074a0942a5818f4dfb9
mickfree <lydoived@gmail.com>
2024-10-14 20:26:30 -0500

    settings.py

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ set -o errexit
 echo "from django.contrib.auth import get_user_model; \
       User = get_user_model(); \
       User.objects.filter(username='admin').exists() or \
-      User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
+      User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
